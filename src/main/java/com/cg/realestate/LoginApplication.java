415fb3bbcfc537da619a4cd269264dac8e958213
Himali Gunjal <himaligunjal31@gmail.com>
2020-05-10 12:25:00 +0530

    logging and swagger added

--- src/main/java/com/cg/realestate/LoginApplication.java
+++ src/main/java/com/cg/realestate/LoginApplication.java
@@ -1,17 +1,51 @@
 package com.cg.realestate;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients("com.cg.realestate")
+@EnableSwagger2
 public class LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
 
+	@Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                   .select()
+                   .paths(PathSelectors.any())
+                   .apis(RequestHandlerSelectors.basePackage("com.cgrealestate"))
+                   .build()
+                   .apiInfo(myApiInfo());
+           }
+	
+	private ApiInfo myApiInfo() {
+        // for version 2.9.1
+        ApiInfo apiInfo=new ApiInfo(
+                "SPRING WITH SWAGGER API",
+                 "API CREATION",
+                 "1.0",
+                 "Free to Use",
+                  new Contact("Himali Gunjal","/property" ,"himaligunjal31@gmail.com"),
+                 "API licence",
+                 "/limits",
+                 Collections.emptyList());
+        return apiInfo;  }
 }
--- src/main/java/com/cg/realestate/controller/LoginController.java
+++ src/main/java/com/cg/realestate/controller/LoginController.java
@@ -1,36 +1,62 @@
 package com.cg.realestate.controller;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cg.realestate.model.User;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
-@CrossOrigin 
+@CrossOrigin
+@Api(value="login")
 public class LoginController {
 
 	@Autowired
 	LoginProxy proxy;
-	
-	@GetMapping(path = "/login/{emailId}/{password}")
-	public boolean login(@PathVariable String emailId, @PathVariable String password ) {
-		
-		List<User> users = proxy.display();
-		
-		for ( User user : users){
 
-			if(user.getEmailId().equals(emailId) && user.getPassword().equals(password)){
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	@GetMapping(path = "/login/{emailId}/{password}")
+	@ApiOperation(value = "login", nickname = "login")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = User.class),
+							@ApiResponse(code = 500, message = "Failure", response = User.class) })
+	public boolean login(@PathVariable String emailId, @PathVariable String password) {
+
+		// List<User> users;// = proxy.display();
+
+		logger.info("log-in");
+		logger.trace(" Inside login() function");
+		logger.error("Error happened at login()");
+
+		String url = "http://localhost:9003/";
+		RestTemplate restTemplate = new RestTemplate();
+		// List<User> user = restTemplate.getClientHttpRequestInitializers(url)
+		ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(url, User[].class);
+		List<User> users = Arrays.asList(responseEntity.getBody());
+
+		for (User user : users) {
+
+			if (user.getEmailId().equals(emailId) && user.getPassword().equals(password)) {
 				System.out.println("inside controller");
 				return true;
 			}
 
 		}
-	    return false;	
+		return false;
 	}
 }
